9723c4a99261f5637c7d7e768fe7523881fd410b
Manuel Bovo <mbovo@facilitylive.com>
2017-07-18 10:43:41 +0200

    API refactoring adding versioning

--- api/router.go
+++ api/router.go
@@ -8,11 +8,11 @@ import (
 
 func GetRouter(core *core.Core, eventChannel chan *logrus.Entry) *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/handle/{autoscaler_name}/{service_name}", Handle(&core.Autoscalers)).Methods("POST")
-	r.HandleFunc("/handle/{autoscaler_name}/{service_name}/{direction}", Handle(&core.Autoscalers)).Methods("POST")
-	r.HandleFunc("/autoscaler", AutoscalerList(core.Autoscalers)).Methods("GET")
-	r.HandleFunc("/health", Health()).Methods("GET")
-	r.HandleFunc("/events", Events(eventChannel)).Methods("GET")
+	r.HandleFunc("/v1/handle/{autoscaler_name}/{service_name}", Handle(&core.Autoscalers)).Methods("POST")
+	r.HandleFunc("/v1/handle/{autoscaler_name}/{service_name}/{direction}", Handle(&core.Autoscalers)).Methods("POST")
+	r.HandleFunc("/v1/autoscaler", AutoscalerList(core.Autoscalers)).Methods("GET")
+	r.HandleFunc("/v1/health", Health()).Methods("GET")
+	r.HandleFunc("/v1/events", Events(eventChannel)).Methods("GET")
 	r.NotFoundHandler = NotFound{}
 	return r
 }
