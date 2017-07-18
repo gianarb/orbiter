0507e1696356a3cba0ff2a44df54cf3f8f22e327
Manuel Bovo <mbovo@facilitylive.com>
2017-07-18 10:29:39 +0200

    Cooldown period

--- api/router.go
+++ api/router.go
@@ -6,10 +6,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func GetRouter(core core.Core, eventChannel chan *logrus.Entry) *mux.Router {
+func GetRouter(core *core.Core, eventChannel chan *logrus.Entry) *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/handle/{autoscaler_name}/{service_name}", Handle(core.Autoscalers)).Methods("POST")
-	r.HandleFunc("/handle/{autoscaler_name}/{service_name}/{direction}", Handle(core.Autoscalers)).Methods("POST")
+	r.HandleFunc("/handle/{autoscaler_name}/{service_name}", Handle(&core.Autoscalers)).Methods("POST")
+	r.HandleFunc("/handle/{autoscaler_name}/{service_name}/{direction}", Handle(&core.Autoscalers)).Methods("POST")
 	r.HandleFunc("/autoscaler", AutoscalerList(core.Autoscalers)).Methods("GET")
 	r.HandleFunc("/health", Health()).Methods("GET")
 	r.HandleFunc("/events", Events(eventChannel)).Methods("GET")
