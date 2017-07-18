e3bad3e35e1c7048d683a8c107a4d5020525394d
Manuel Bovo <mbovo@facilitylive.com>
2017-07-18 17:45:00 +0200

    Fixing autoscaler_test

--- autoscaler/autoscaler_test.go
+++ autoscaler/autoscaler_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestFakeProvider(t *testing.T) {
 	var p Provider
-	a := NewAutoscaler(p, "fgaerge", 3, 4)
+	a := NewAutoscaler(p, "fgaerge", 3, 4, 30)
 	if a.provider != p {
 		t.Fail()
 	}
