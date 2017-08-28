fd1a0b1a2c27314545777f8239a27b726d94c320
Gianluca Arbezzano <gianarb92@gmail.com>
2017-08-29 00:28:04 +0200

    Removed multi provider and yml configuration
    This PR removes code not in the project's scope anymore.
    It removes yml configuration file. And digitalocean provider.
    
    Oribiter will be a Docker Swarm specific autoscaler.
    
    Fixed #17

--- core/daemon.go
+++ core/daemon.go
@@ -1,27 +1,9 @@
 package core
 
 import (
-	"fmt"
-
 	"github.com/gianarb/orbiter/autoscaler"
-	"github.com/gianarb/orbiter/provider"
 )
 
 type Core struct {
 	Autoscalers autoscaler.Autoscalers
-}
-
-func NewCoreByConfig(c map[string]AutoscalerConf, core *Core) error {
-	scalers := autoscaler.Autoscalers{}
-	for scalerName, scaler := range c {
-		p, err := provider.NewProvider(scaler.Provider, scaler.Parameters)
-		if err != nil {
-			return err
-		}
-		for serviceName, policy := range scaler.Policies {
-			scalers[fmt.Sprintf("%s/%s", scalerName, serviceName)] = autoscaler.NewAutoscaler(p, serviceName, policy.Up, policy.Down, policy.CoolDown)
-		}
-	}
-	core.Autoscalers = scalers
-	return nil
 }
